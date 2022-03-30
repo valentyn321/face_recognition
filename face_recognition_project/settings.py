b02c627ce264f069fe63dcf796bf768aafe18293
Valentyn <valentyncherkasov24@gmail.com>
2022-03-30 14:19:42 +0300

    add login/logout endpoints

--- face_recognition_project/settings.py
+++ face_recognition_project/settings.py
@@ -127,4 +127,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
-# LOGIN_REDIRECT_URL = "recognition/"
+LOGIN_URL = "login"
+
+LOGIN_REDIRECT_URL = "face_detection"
--- face_recognition_project/urls.py
+++ face_recognition_project/urls.py
@@ -1,11 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 
 urlpatterns = [
-    path("accounts/", include("django.contrib.auth.urls")),
     path("admin/", admin.site.urls),
+    path("login/", auth_views.LoginView.as_view(template_name="accounts/login.html"), name="login"),
+    path("logout/", auth_views.LogoutView.as_view(template_name="accounts/logout.html"), name="logout"),
     path("recognition/", include("recognition.urls")),
 ]
 if settings.DEBUG:
