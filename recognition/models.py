689c249e82f239b05edc2cbcc7bcece3c5a7c9f1
Valentyn <valentyncherkasov24@gmail.com>
2022-04-14 12:32:39 +0300

    modify Image model by adding boolean field, which represent face's presence

--- recognition/models.py
+++ recognition/models.py
@@ -7,6 +7,7 @@ User = settings.AUTH_USER_MODEL
 class Image(models.Model):
     input_url = models.ImageField()
     output_url = models.ImageField(null=True, blank=True)
+    faces_presence = models.BooleanField(default=0)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
 
